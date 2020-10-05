b9bcd22e5882532cc245ae6090bdf7c3b7e4408a
tueda <ueo@ueo.co.jp>
2020-10-05 15:39:52 +0900

    maven repository for apache poi 4.1.2 adapted is moved to gh-pages-dev.

--- blanco/main/blanco/valueobject/BlancoValueObjectConstants.java
+++ blanco/main/blanco/valueobject/BlancoValueObjectConstants.java
@@ -20,7 +20,7 @@ public class BlancoValueObjectConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "2.0.4";
+    public static final String VERSION = "2.1.1";
 
     /**
      * 項目番号:4<br>
